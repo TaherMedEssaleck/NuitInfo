f4aa4685ed15926438a6ca0ef2f765ac100877d8
Taher <22041@esp.mr>
2024-12-06 01:11:11 +0000

    nav

--- nuit/src/app/app-routing.module.ts
+++ nuit/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PodcastsComponent } from './podcasts/podcasts.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'podcasts', component: PodcastsComponent },
+  {path:'',component:HomeComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- nuit/src/app/app.module.ts
+++ nuit/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageTextComponent } from './image-text/image-text.component';
+import { PodcastsComponent } from './podcasts/podcasts.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageTextComponent
+    ImageTextComponent,
+    PodcastsComponent,
+    HomeComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
