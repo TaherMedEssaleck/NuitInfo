9f46e5f4233e66640496fa82d5cb662cf52cbcf0
rmdnhub <rmdn483@gmail.com>
2024-12-06 06:18:37 +0100

    podcats improvement

--- nuit/src/app/podcasts/podcasts.component.ts
+++ nuit/src/app/podcasts/podcasts.component.ts
@@ -6,29 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./podcasts.component.css']
 })
 export class PodcastsComponent {
+  // Liste des podcasts avec l'option audio ou vidéo
   podcasts = [
-    { title: 'Frederic Le Moigne - Part 1', url: 'assets/Fredeeric_Le_Moigne.mp3' },
-    { title: 'Florian Sevellec - Part 2', url: 'assets/s_Florian_Sevellec.mp3' }
-
+    { id: 1, title: 'Florian Sevellec - Part 1', url: 'assets/s_Florian_Sevellec.mp4', isVideo: true },
+    { id: 2, title: 'Frederic Le Moigne - Part 1', url: 'assets/s_Frederic_Le_Moigne_-_Part_1.mp4', isVideo: true },
+    { id: 3, title: 'Florian Sevellec - Oona Layolle', url: 'assets/s_Florian_Sevellec_-_Oona_Layolle.mp4', isVideo: true },
+    { id: 4, title: 'Frederic Le Moigne - Part 2', url: 'assets/s_Frederic_Le_Moigne_-_Part_2.mp4', isVideo: true },
+    { id: 5, title: 'Frederic Le Moigne - Part 1 Audio', url: 'assets/Fredeeric_Le_Moigne.mp3', isVideo: false },
+    { id: 6, title: 'Florian Sevellec - Part 2 Audio', url: 'assets/s_Florian_Sevellec.mp3', isVideo: false }
   ];
 
   currentPodcast: any = null;
-  jingle = 'assets/jingle.mp3';
 
+  // Gère la lecture des podcasts
   playPodcast(podcast: any) {
     this.currentPodcast = podcast;
-    const audioPlayer = document.querySelector('audio');
-    if (audioPlayer) {
-      audioPlayer.src = this.jingle;
-      audioPlayer.play();
-      audioPlayer.onended = () => {
-        audioPlayer.src = podcast.url;
-        audioPlayer.play();
-      };
-    }
   }
 
-  onAudioEnd() {
+  // Fonction exécutée à la fin de la lecture de l'audio ou vidéo
+  onMediaEnd() {
     this.currentPodcast = null;
   }
 }
